ceaa8bef7151e74db9b18828b4ec2664bcc42d53
marcdevceo <marcdevceo@icloud.com>
2025-06-19 03:45:58 -0400

    add get and delete routes

--- src/db/schema.js
+++ src/db/schema.js
@@ -2,8 +2,8 @@ import { pgTable, serial, text, timestamp, integer } from "drizzle-orm/pg-core";
 
 export const favoritesTable = pgTable("favorites", {
     id: serial("id").primaryKey(),
-    userID: text("user_id").notNull(),
-    recipeID: integer("recipe_id").notNull(),
+    userId: text("user_id").notNull(),
+    recipeId: integer("recipe_id").notNull(),
     title: text("title").notNull(),
     image: text("image"),
     cookTime: text("cook_time"),
